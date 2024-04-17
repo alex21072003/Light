c35e18b68833a7d8579d8a5194a686840016f86a
alex21072003 <alexkarpinskiy21@gmail.com>
2024-04-17 11:44:05 +0300

    My first code

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int maxWeight = 5000;
+        int minCost = 1000;
+
+        int weight = 6000;
+        int cost = 1700;
+        if (weight <= maxWeight && cost <= minCost) {
+
+            System.out.println("Your rent is chipper");
+
+        }
+    }
+}
